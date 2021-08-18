1bb300ab0d8ee6a71acdf7bd755b039b0903861e
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-08-18 20:34:18 -0300

    show specific + show all user related requests + accept request

--- src/controllers/RequestController.ts
+++ src/controllers/RequestController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
-import { CreateRequestService } from "../services/CreateRequestService";
+import { AcceptRequestService } from "../services/Request/AcceptRequestService";
+import { CreateRequestService } from "../services/Request/CreateRequestService";
+import { FindRequestService } from "../services/Request/FindRequestService";
+import { IndexRequestService } from "../services/Request/IndexRequestService";
 
 class RequestController {
   async store(req: Request, res: Response) {
@@ -12,7 +15,42 @@ class RequestController {
       email,
       value,
       description,
-      from: user_id,
+      self: user_id,
+    });
+
+    return res.json(request);
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+    const { user_id } = req;
+
+    const findRequestService = new FindRequestService();
+
+    const request = await findRequestService.execute({ id, self: user_id });
+
+    return res.json(request);
+  }
+
+  async index(req: Request, res: Response) {
+    const { user_id } = req;
+
+    const indexRequestService = new IndexRequestService();
+
+    const request = await indexRequestService.execute({ self: user_id });
+
+    return res.json(request);
+  }
+
+  async accept(req: Request, res: Response) {
+    const { id } = req.params;
+    const { user_id } = req;
+
+    const acceptRequestService = new AcceptRequestService();
+
+    const request = await acceptRequestService.execute({
+      self: user_id,
+      id,
     });
 
     return res.json(request);
