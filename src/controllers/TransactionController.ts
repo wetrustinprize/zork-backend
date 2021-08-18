495b96eeb7606062d018fe63f797bd53a980f7b0
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-08-18 20:34:29 -0300

    moved files change

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { CreateTransactionService } from "../services/CreateTransactionService";
-import { FindTransactionService } from "../services/FindTransactionService";
+import { CreateTransactionService } from "../services/Transaction/CreateTransactionService";
+import { FindTransactionService } from "../services/Transaction/FindTransactionService";
 
 class TransactionController {
   async store(req: Request, res: Response) {
@@ -19,14 +19,17 @@ class TransactionController {
   }
 
   async show(req: Request, res: Response) {
-    const { id } = req.params
-    const { user_id } = req
+    const { id } = req.params;
+    const { user_id } = req;
 
-    const findTransactionService = new FindTransactionService()
+    const findTransactionService = new FindTransactionService();
 
-    const transaction = await findTransactionService.execute({transaction_id: id, user_id})
+    const transaction = await findTransactionService.execute({
+      transaction_id: id,
+      user_id,
+    });
 
-    return res.json(transaction)
+    return res.json(transaction);
   }
 }
 
